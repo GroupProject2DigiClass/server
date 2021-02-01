761ce94aff33d49d8193af09c70a08fc77e44444
Niket-iiitu <niketagrawal777@gmail.com>
2021-02-01 13:01:43 +0530

    fix added

--- S3_routes/posts.js
+++ S3_routes/posts.js
@@ -1,21 +1,33 @@
 const express = require("express");
 
-const { addUser, addItem } = require("../S1_controllers/userController");
+const {
+  addUser,
+  addItem,
+  getUser,
+} = require("../S1_controllers/userController");
 
 const router = express.Router();
 
-const { searchit,findcityname,updateliveproduct}  =require( '../S1_controllers/postsController');
+const {
+  searchit,
+  findcityname,
+  updateliveproduct,
+} = require("../S1_controllers/postsController");
 
 router.post("/addUser", addUser);
 
 router.post("/addItem", addItem);
 
-router.get('/search', searchit);
+router.get("/search", searchit);
 //router.post('/', createPost);
-router.post('/search',searchit);
+router.post("/search", searchit);
 
-router.post('/cityname',findcityname);
+router.post("/cityname", findcityname);
 
-router.post('/updateliveproduct',updateliveproduct);
+router.post("/updateliveproduct", updateliveproduct);
+
+router.get("/start", getUser);
+
+router.post("/start", getUser);
 
 module.exports = router;
