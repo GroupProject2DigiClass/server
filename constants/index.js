7112c83f245d56315ecff6d59dbb5872a3e05526
Niket-iiitu <niketagrawal777@gmail.com>
2021-02-10 18:03:59 +0530

    form schema added

---
+++ constants/index.js
@@ -0,0 +1,31 @@
+InstituteTypeOptions = [
+  "COLLEGE",
+  "PRIMARY_SCHOOL",
+  "SECONDARY_SCHOOL",
+  "UNIVERSITY",
+];
+
+YearTypeOptions = ["YEAR", "SEMESTER", "CLASS"];
+
+SectionTypeOption = ["SECTION", "BRANCH"];
+
+AdminDesignationOptions = ["DIRECTOR", "PRINCIPLE"];
+
+StudentRankOptions = ["STUDENT", "CR"];
+
+TeacherRankOptions = ["TEACHER", "HOD"];
+
+AdminRankOptions = ["ADMIN"];
+
+GenderOptions = ["MALE", "FEMALE"];
+
+module.exports = {
+  InstituteTypeOptions,
+  YearTypeOptions,
+  SectionTypeOption,
+  AdminDesignationOptions,
+  StudentRankOptions,
+  TeacherRankOptions,
+  GenderOptions,
+  AdminRankOptions,
+};
