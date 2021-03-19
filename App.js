3da59dcb7a2d251079889419609dd5d0363b9022
adi <19106@iiitu.ac.in>
2021-03-19 11:52:17 +0530

    add and get data aout class

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/data", router);
+app.use("/makeclass", router);
 
 app.get("/", (req, res) => {
   res.send("Hello to Kamal API");
