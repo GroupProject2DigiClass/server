9d929b3496d401010589b24a8192d507a1623f61
Niket-iiitu <niketagrawal777@gmail.com>
2021-06-27 15:36:40 +0530

    add data

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const express = require("express");
+const router = require("./S3_routes/posts");
 const dotenv = require("dotenv");
 const app = express();
-const classRouter = require("./routes/classRout");
-const lectureRouter = require("./routes/lectureRout");
-const chatRouter = require("./routes/chatRout");
 
 dotenv.config();
 app.use(express.json());
@@ -14,16 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/makeclass", classRouter);
-app.use("/makelecture", lectureRouter);
-app.use("/makechat", chatRouter);
+app.use("/makeclass", router);
+
 app.get("/", (req, res) => {
   res.send("Hello to Kamal API");
 });
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
 
 const PORT = process.env.PORT || 5005;
 
